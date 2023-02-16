967bb2af04f03dc0b21d92b9e5ec2542f83aef4f
Petkunushka <tatiana30071993@gmail.com>
2023-02-16 13:09:35 +0300

    Update timer.js file

--- timer.js
+++ timer.js
@@ -6,5 +6,19 @@ const secondsElem = document.querySelector('time__seconds');
 
 const showTime = (seconds) => {
     minutesElem.textContent = Math.floor(seconds/60);
-    
+    secondsElem.textContent = seconds % 60;
+}
+
+export const startTimer = () => {
+    state.timeLeft -=1;
+
+    showTime(state.timeLeft);
+
+    if (state.timeLeft > 0 && state.isActive) {
+        state.timerId = setTimeout(startTimer, 1000);
+    }
+
+    if (state.timerLeft <= 0) {
+        alarm();
+    }
 }
